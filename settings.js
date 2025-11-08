837084548d5a7ce6ac4e7bc43944962bc50aba7c
Mrwaxly <burakyvz56@gmail.com>
2025-11-09 01:19:45 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
+global.ownernomer = "905352586556"
 global.ownername = "🐛 MR WASI "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
